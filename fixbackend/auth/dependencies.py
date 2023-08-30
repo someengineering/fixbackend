69b4f52d71c4265199fda6c48d194796039e3891
Nikita Melkozerov <nikita@some.engineering>
2023-08-30 16:08:55 +0000

    fix fastapi_users dependency

--- fixbackend/auth/dependencies.py
+++ fixbackend/auth/dependencies.py
@@ -1,9 +1,15 @@
 
 from typing import Annotated
 from fastapi import Depends
+import uuid
 from fastapi.security.http import HTTPBearer
-from fixbackend.auth.users import fastapi_users
 from fixbackend.auth.models import User
+from fastapi_users import FastAPIUsers
+from fixbackend.auth.models import User
+from fixbackend.auth.user_manager import get_user_manager
+from fixbackend.auth.jwt import jwt_auth_backend
+
+fastapi_users = FastAPIUsers[User, uuid.UUID](get_user_manager, [jwt_auth_backend])
 
 
 current_active_user = fastapi_users.current_user(active=True)
