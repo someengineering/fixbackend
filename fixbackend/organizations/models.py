f9d9c45e2d0cae8de71f716a81563db0419a71ed
Nikita Melkozerov <nikita@some.engineering>
2023-09-21 16:10:56 +0200

    Add an explicit tenant id (#37)

--- fixbackend/organizations/models.py
+++ fixbackend/organizations/models.py
@@ -31,6 +31,7 @@ class Organization(Base):
     slug: Mapped[str] = mapped_column(String(length=320), unique=True, nullable=False)
     name: Mapped[str] = mapped_column(String(length=320), nullable=False)
     external_id: Mapped[uuid.UUID] = mapped_column(GUID, default=uuid.uuid4, nullable=False)
+    tenant_id: Mapped[uuid.UUID] = mapped_column(GUID, default=uuid.uuid4, nullable=False)
     owners: Mapped[List["OrganizationOwners"]] = relationship(back_populates="organization")
     members: Mapped[List["OrganizationMembers"]] = relationship(back_populates="organization")
 
