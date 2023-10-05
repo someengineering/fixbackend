d7f5e13affa60b7690f66c0e9a60ade5451da564
Matthias Veit <aquamatthias@users.noreply.github.com>
2023-10-05 09:52:58 +0200

    [feat] Implement a SQS publisher and listener (#78)

--- noxfile.py
+++ noxfile.py
@@ -13,7 +13,6 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from nox_poetry import session, Session
-from tempfile import TemporaryDirectory
 
 locations = ["fixbackend", "tests", "noxfile.py"]
 
@@ -37,19 +36,7 @@ def flake8(session: Session) -> None:
 @session(python=["3.11"])
 def test(session: Session) -> None:
     args = session.posargs or ["--cov"]
-    # workaround for CI to create a wheel outside the project folder
-    with TemporaryDirectory() as tmpdir:
-        session.run("cp", "-R", ".", str(tmpdir), external=True)
-        with session.chdir(tmpdir):
-            session.poetry.installroot()
-    session.install(
-        "pytest",
-        "pytest-cov",
-        "pytest-asyncio",
-        "sqlalchemy-utils",
-        "git+https://github.com/frankie567/httpx-ws.git@f9f2555603f40053f84adf6890e6cda245294b18#egg=httpx-ws",
-        ".",
-    )
+    session.run_always("poetry", "install", external=True)
     session.run("pytest", *args)
 
 
