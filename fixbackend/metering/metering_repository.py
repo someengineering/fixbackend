beed3dff60f2aca7bcec090b9a57917f6f2b9a5e
Matthias Veit <matthias_veit@yahoo.de>
2023-10-09 16:53:06 +0200

    [fix] Define columns as nullable

--- fixbackend/metering/metering_repository.py
+++ fixbackend/metering/metering_repository.py
@@ -33,8 +33,8 @@ class MeteringRecordEntity(Base):
 
     id: Mapped[UUID] = mapped_column(GUID, primary_key=True)
     tenant_id: Mapped[WorkspaceId] = mapped_column(GUID, nullable=False, index=True)
-    cloud: Mapped[str] = mapped_column(String(10), nullable=False, default="aws")
-    account_id: Mapped[str] = mapped_column(String(36), nullable=False, default="")
+    cloud: Mapped[str] = mapped_column(String(10), nullable=True, default="aws")
+    account_id: Mapped[str] = mapped_column(String(36), nullable=True, default="")
     account_name: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)
     timestamp: Mapped[datetime] = mapped_column(UTCDateTime, nullable=False, index=True)
     job_id: Mapped[str] = mapped_column(String(36), nullable=False)
