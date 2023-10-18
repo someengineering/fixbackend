672c77d300972528b3ed5084472b828d36d69ab4
Matthias Veit <aquamatthias@users.noreply.github.com>
2023-10-18 10:17:35 +0200

    [fix] handle cross-origin post (#113)

--- fixbackend/subscription/router.py
+++ fixbackend/subscription/router.py
@@ -19,54 +19,52 @@
 #
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from functools import partial
-from typing import Callable
 
 from fastapi import APIRouter, Form, Cookie, Response, Request
 from starlette.responses import RedirectResponse
 
-from fixbackend.auth.depedencies import OptionalAuthenticatedUser, AuthenticatedUser
+from fixbackend.auth.depedencies import OptionalAuthenticatedUser
 from fixbackend.dependencies import FixDependencies, ServiceNames as SN
 from fixbackend.subscription.aws_marketplace import AwsMarketplaceHandler
 
 AddUrlName = "aws-marketplace-subscription-add"
+MarketplaceTokenCookie = "fix-aws-marketplace-token"
 
 
 def subscription_router(deps: FixDependencies) -> APIRouter:
     router = APIRouter()
-    market_place_handler: Callable[[], AwsMarketplaceHandler] = partial(  # type: ignore
-        deps.service, SN.aws_marketplace_handler, AwsMarketplaceHandler
-    )
+
+    def market_place_handler() -> AwsMarketplaceHandler:
+        return deps.service(SN.aws_marketplace_handler, AwsMarketplaceHandler)
 
     # Attention: Changing this route will break the AWS Marketplace integration!
     @router.post("/cloud/callbacks/aws/marketplace")
     async def aws_marketplace_fulfillment(
         request: Request,
-        maybe_user: OptionalAuthenticatedUser,
         x_amzn_marketplace_token: str = Form(alias="x-amzn-marketplace-token"),
     ) -> Response:
-        if user := maybe_user:
-            # add marketplace subscription
-            await market_place_handler().subscribed(user, x_amzn_marketplace_token)
-            # load the app and show a message
-            return RedirectResponse("/?message=aws-marketplace-subscribed")
-        else:
-            add_url = request.scope["router"].url_path_for(AddUrlName)
-            response = RedirectResponse(f"/auth/login?returnUrl={add_url}")
-            response.set_cookie("fix-aws-marketplace-token", x_amzn_marketplace_token, secure=True, httponly=True)
-            return response
+        # Cross-Origin Post Request: we will not receive our auth cookie here.
+        # Store the token in a cookie and redirect to the marketplace add page.
+        response = RedirectResponse(request.scope["router"].url_path_for(AddUrlName))
+        response.set_cookie(MarketplaceTokenCookie, x_amzn_marketplace_token, secure=True, httponly=True)
+        return response
 
     @router.get("/subscriptions/aws/marketplace/add", response_model=None, name=AddUrlName)
     async def aws_marketplace_fulfillment_after_login(
-        user: AuthenticatedUser, fix_aws_marketplace_token: str = Cookie(None, alias="fix-aws-marketplace-token")
+        request: Request,
+        maybe_user: OptionalAuthenticatedUser,
+        fix_aws_marketplace_token: str = Cookie(None, alias="fix-aws-marketplace-token"),
     ) -> Response:
-        if fix_aws_marketplace_token is not None:
+        if maybe_user is None:  # not logged in
+            add_url = request.scope["router"].url_path_for(AddUrlName)
+            return RedirectResponse(f"/auth/login?returnUrl={add_url}")
+        elif (user := maybe_user) and fix_aws_marketplace_token is not None:  # logged in and token present
             await market_place_handler().subscribed(user, fix_aws_marketplace_token)
             # load the app and show a message
             response = RedirectResponse("/?message=aws-marketplace-subscribed")
-            response.set_cookie("fix-aws-marketplace-token", "", expires=0)  # delete the cookie
+            response.set_cookie(MarketplaceTokenCookie, expires=0, secure=True, httponly=True)  # delete the cookie
             return response
-        else:
+        else:  # something went wrong
             raise ValueError("No AWS token found!")
 
     return router
