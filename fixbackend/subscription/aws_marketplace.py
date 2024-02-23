4dc7cbc324dc9190371197db8d88295bb58a80a0
Matthias Veit <matthias_veit@yahoo.de>
2024-02-23 12:58:02 +0100

    [fix] adjust sqs defaults

--- fixbackend/subscription/aws_marketplace.py
+++ fixbackend/subscription/aws_marketplace.py
@@ -90,7 +90,7 @@ class AwsMarketplaceHandler(Service):
                 self.handle_message,
                 consider_failed_after=timedelta(minutes=5),
                 max_nr_of_messages_in_one_batch=1,
-                wait_for_new_messages_to_arrive=timedelta(seconds=10),
+                wait_for_new_messages_to_arrive=timedelta(seconds=20),
             )
             if sqs_queue_url is not None
             else None
