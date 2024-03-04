6f0cd45ca1b65e3e99aeab6c945795d203576ba4
Nikita Melkozerov <nikita@some.engineering>
2024-03-04 17:38:22 +0000

    more logging

--- fixbackend/subscription/aws_marketplace.py
+++ fixbackend/subscription/aws_marketplace.py
@@ -116,7 +116,7 @@ class AwsMarketplaceHandler(Service):
         log.info(f"AWS Marketplace subscription for user {user.email} with token {token}")
         # Get the related data from AWS. Will throw in case of an error.
         customer_data = self.marketplace_client.resolve_customer(RegistrationToken=token)
-        log.debug(f"AWS Marketplace user {user.email} got customer data: {customer_data}")
+        log.info(f"AWS Marketplace user {user.email} got customer data: {customer_data}")
         product_code = customer_data["ProductCode"]
         customer_identifier = customer_data["CustomerIdentifier"]
         customer_aws_account_id = customer_data["CustomerAWSAccountId"]
@@ -136,7 +136,9 @@ class AwsMarketplaceHandler(Service):
 
         # only create a new subscription if there is no existing one
         if existing := await self.subscription_repo.aws_marketplace_subscription(user.id, customer_identifier):
-            log.debug(f"AWS Marketplace user {user.email}: return existing subscription")
+            log.info(
+                f"AWS Marketplace user {user.email}: return existing subscription, user_id: {existing}, customer_identifier: {customer_identifier}"
+            )
             return existing, workspace_assigned
         else:
             subscription = AwsMarketplaceSubscription(
