bc314770d38791f843ab065117f6e2a606439710
Nikita Melkozerov <nikita@some.engineering>
2024-04-24 16:46:19 +0100

    Do not let users update their own roles (#412)

--- tests/fixbackend/permissions/roles_router_test.py
+++ tests/fixbackend/permissions/roles_router_test.py
@@ -106,15 +106,31 @@ async def test_list_roles(
 async def test_update_roles(
     client: AsyncClient,
     workspace: Workspace,
+    user_repository: UserRepository,
     user: User,
 ) -> None:
 
-    payload = {"user_id": str(user.id), "member": False, "admin": False, "owner": False, "billing_admin": True}
+    user_dict = {
+        "email": "foo_bar123@bar.com",
+        "hashed_password": "notreallyhashed",
+        "is_verified": True,
+    }
 
-    response = await client.put(f"/api/workspaces/{workspace.id}/roles/{user.id}", json=payload)
+    someone_else = await user_repository.create(user_dict)
+
+    own_payload = {"user_id": str(user.id), "member": False, "admin": False, "owner": False, "billing_admin": True}
+
+    # self update is not allowed
+    response = await client.put(f"/api/workspaces/{workspace.id}/roles/{user.id}", json=own_payload)
+    assert response.status_code == 403
+
+    # update someone else is fine
+    payload = {"user_id": str(someone_else.id), "member": False, "admin": False, "owner": False, "billing_admin": True}
+
+    response = await client.put(f"/api/workspaces/{workspace.id}/roles/{someone_else.id}", json=payload)
     assert response.status_code == 200
     assert response.json() == {
-        "user_id": str(user.id),
+        "user_id": str(someone_else.id),
         "workspace_id": str(workspace.id),
         "member": False,
         "admin": False,
