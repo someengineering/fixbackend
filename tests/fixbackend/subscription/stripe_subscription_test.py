e836273b48b18387e591c2e23c947019c6e6344b
Nikita Melkozerov <nikita@some.engineering>
2024-05-13 11:25:57 +0000

    chore: Remove unused import in stripe_subscription_test.py

--- tests/fixbackend/subscription/stripe_subscription_test.py
+++ tests/fixbackend/subscription/stripe_subscription_test.py
@@ -12,7 +12,6 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from datetime import timedelta
-from turtle import up
 from typing import Dict, Any
 
 import stripe
