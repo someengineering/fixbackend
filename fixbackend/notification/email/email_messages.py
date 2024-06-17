92c5296c66e23cac9c75291efc0fbc552c7c45f5
Matthias Veit <matthias_veit@yahoo.de>
2024-06-17 17:42:07 +0200

    [fix] Show 0-3 as such

--- fixbackend/notification/email/email_messages.py
+++ fixbackend/notification/email/email_messages.py
@@ -24,14 +24,11 @@ from fixbackend.utils import uid
 from fixbackend.workspaces.models import Workspace
 
 TemplatesPath = Path(__file__).parent / "templates"
-_readable_numbers = {0: "zero", 1: "one", 2: "two", 3: "three"}
 _bytes_power: Dict[int, Optional[str]] = {5: "PiB", 4: "TiB", 3: "GiB", 2: "MiB", 1: "KiB", 0: "B"}
 _decimal_power: Dict[int, Optional[str]] = {5: "P", 4: "T", 3: "G", 2: "M", 1: "K", 0: None}
 
 
 def _readable_number(number: int, *, with_sign: Optional[bool] = None) -> str:
-    if num := _readable_numbers.get(number):
-        return num
     return _readable_unit(1000, _decimal_power, None, number, with_sign)
 
 
