444fae13b499026b9525ccf6ce365a0fc9356846
Nikita Melkozerov <nikita@some.engineering>
2024-08-01 11:28:24 +0200

    Post collect job support (#580)

--- fixbackend/collect/collect_queue.py
+++ fixbackend/collect/collect_queue.py
@@ -14,7 +14,7 @@
 import logging
 from abc import ABC, abstractmethod
 from datetime import timedelta
-from typing import Dict, Optional, ClassVar
+from typing import Dict, Optional, ClassVar, List
 from uuid import UUID
 
 from arq import ArqRedis
@@ -23,7 +23,7 @@ from cattrs.preconf.json import make_converter
 from fixcloudutils.types import Json
 
 from fixbackend.graph_db.models import GraphDatabaseAccess
-from fixbackend.ids import CloudAccountId, AwsARN, ExternalId
+from fixbackend.ids import CloudAccountId, AwsARN, ExternalId, CloudName, TaskId
 
 log = logging.getLogger(__name__)
 
@@ -74,6 +74,17 @@ class AzureSubscriptionInformation(AccountInformation):
     collect_microsoft_graph: bool = False  # Whether to collect Microsoft Graph data
 
 
+@define
+class PostCollectAccountInfo:
+    cloud: CloudName
+    account_id: CloudAccountId
+    task_id: TaskId
+
+    def to_json(self) -> Json:
+        js: Json = json_converter.unstructure(self)
+        return js
+
+
 class CollectQueue(ABC):
     @abstractmethod
     async def enqueue(
@@ -89,6 +100,33 @@ class CollectQueue(ABC):
     ) -> None:
         """
         Enqueue a collect job. This method will only put the job into the queue.
+
+        :param db: The database access configuration.
+        :param account: The account information to collect.
+        :param env: The environment variables to pass to the worker.
+        :param job_id: Globally unique identifier of this job.
+               If provided and another job exists with the same id, the job will not be enqueued.
+        :param wait_until_done: If true, this method will wait until the job is completed.
+               This number is used to determine the assigned resources for this job (cpu / memory).
+        :param defer_by: defer the job start by this amount of time.
+        :param retry_failed_for: Retry failed jobs for this amount of time.
+        :return: None
+        """
+
+    @abstractmethod
+    async def enqueue_post_collect(
+        self,
+        db: GraphDatabaseAccess,
+        accounts_collected: List[PostCollectAccountInfo],
+        *,
+        env: Optional[Dict[str, str]] = None,
+        job_id: Optional[str] = None,
+        wait_until_done: bool = False,
+        defer_by: Optional[timedelta] = None,
+        retry_failed_for: Optional[timedelta] = None,
+    ) -> None:
+        """
+        Enqueue a post-collect job. This method will only put the job into the queue.
 
         :param db: The database access configuration.
         :param account: The account information to collect.
@@ -129,6 +167,36 @@ class RedisCollectQueue(CollectQueue):
             retry_failed_for_seconds=retry_failed_for.total_seconds() if retry_failed_for else None,
         )
         job = await self.arq.enqueue_job("collect", collect_job, _job_id=job_id, _defer_by=defer_by)
+        if job is None:
+            raise JobAlreadyEnqueued(f"Failed to enqueue collect job {job_id}")
+        log.info(f"Enqueuing collect job {job.job_id} for tenant={db.workspace_id}")
+        if wait_until_done:
+            # this will either return none or throw an exception (reraised from the worker)
+            log.debug("Waiting for collect job to finish.")
+            await job.result()
+
+    async def enqueue_post_collect(
+        self,
+        db: GraphDatabaseAccess,
+        accounts_collected: List[PostCollectAccountInfo],
+        *,
+        env: Optional[Dict[str, str]] = None,
+        job_id: Optional[str] = None,
+        wait_until_done: bool = False,
+        defer_by: Optional[timedelta] = None,
+        retry_failed_for: Optional[timedelta] = None,
+    ) -> None:
+        collect_job = dict(
+            tenant_id=str(db.workspace_id),
+            graphdb_server=db.server,
+            graphdb_database=db.database,
+            graphdb_username=db.username,
+            graphdb_password=db.password,
+            accounts_collected=[info.to_json() for info in accounts_collected],
+            env=env or {},
+            retry_failed_for_seconds=retry_failed_for.total_seconds() if retry_failed_for else None,
+        )
+        job = await self.arq.enqueue_job("post-collect", collect_job, _job_id=job_id, _defer_by=defer_by)
         if job is None:
             raise JobAlreadyEnqueued(f"Failed to enqueue collect job {job_id}")
         log.info(f"Enqueuing collect job {job.job_id} for tenant={db.workspace_id}")
