f8f25845862201a70aa68dfe25096311fc1edec5
Nikita Melkozerov <nikita@some.engineering>
2024-08-09 18:23:00 +0200

    Make sure benchmark info is a dict (#595)

--- fixbackend/inventory/inventory_service.py
+++ fixbackend/inventory/inventory_service.py
@@ -477,8 +477,12 @@ class InventoryService(Service):
                         failed_checks: Dict[ReportSeverity, int] = {}
                         failed_resource_checks: Dict[ReportSeverity, int] = {}
 
+                        if not isinstance(info, dict):
+                            continue
+
                         # "failed": { "high": { "checks": 6, "resources": 14 }, "low": { "checks": 4, "resources": 7 }}
                         for severity, severity_info in info.get("failed", {}).items():
+
                             checks = severity_info["checks"]
                             failed_checks[severity] = checks
                             failed_resource_checks[severity] = severity_info["resources"]
